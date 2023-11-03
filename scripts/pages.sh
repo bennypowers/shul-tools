17b8acd7b74ba6725c84059fb7e349b76cda67f6
Benny Powers <web@bennypowers.com>
2023-11-03 15:47:11 +0200

    chore: pages

--- scripts/pages.sh
+++ scripts/pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir -p public/node_modules
+mkdir -p public/node_modules/@shul/elements
 cp demo/* public/
 cp -r node_modules public/
-cp -r elements public/node_modules/
-sed -i 's/\.\.\/elements/\.\/node_modules\/elements/' public/index.html
+cp -r elements public/node_modules/@shul/
 sed -i 's/\.\.\/node_modules/\.\/node_modules/' public/index.html
+sed -i 's/\.\.\/elements/\.\/node_modules\/@shul/elements/' public/index.html
